0f55e06a45dfe08f456cbcbc090a0192536a2978
jitendra4545 <jitendra45.js45@gmail.com>
2022-08-26 09:39:33 +0530

    added

---
+++ Scripts/men saloon page.js
@@ -0,0 +1,290 @@
+let menssalon=[
+  {
+  "name":"Haircut + Shave + Free 10 min head massage",
+  "rating":"4.83 (256.4K)",
+  "price":"₹399",
+  "time":"50 mins",
+  "free":"Get Free",
+  "description":"Clean Shave/ Moustache Grooming",
+  "desc2":"Men's Haircut",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Haircut + Beard grooming + Free 10 min head massage",
+  "rating":"4.84 (271.4K)",
+  "price":"₹449",
+  "time":"55 mins",
+  "free":"Get Free",
+  "description":"Men's Haircut",
+  "desc2":"Beard Shape & Style",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Haircut + Massage",
+  "rating":"4.83 (235.7K)",
+  "price":"₹449",
+  "time":"50 mins",
+  "description":"Men's Haircut",
+  "desc2":"20 min Head Massage",
+  "editpackage":"Edit your package",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Father & Kid's Haircut",
+  "rating":"4.83 (212K)",
+  "price":"₹599",
+  "time":"1 hr 10 mins",
+  "description":"Men's Haircut",
+  "desc2":"Kids Haircut (Boys)",
+  "editpackage":"Edit your package",
+  "desc3":"Kids Haircut (Boys)",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Haircut + Face Care",
+  "rating":"4.82 (239.5K)",
+  "price":"₹649",
+  "time":"50 mins",
+  "description":"Face & Neck Detan Pack",
+  "desc2":"Men's Haircut",
+  "editpackage":"Edit your package",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Haircut + Hair Color",
+  "rating":"4.83 (229.2K)",
+  "price":"₹549",
+  "time":"60 mins",
+  "description":"Garnier Color Naturals",
+  "desc2":"Men's Haircut",
+  "editpackage":"Edit your package",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Make Your Own Package",
+  "rating":"4.82 (128.6K)",
+  "price":"₹1,249",
+  "time":"2 hrs 5 mins",
+  "description":"Beard Shape & Style",
+  "desc2":"Garnier Color Naturals",
+  "editpackage":"Edit your package",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Haircut for Men",
+  "rating":"4.84 (456.8K)",
+  "price":"₹249",
+  "time":"30 mins",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1651735873259-9ad829.png",
+  "package":"BESTSELLER"
+  },
+  {
+  "name":"Haircut for 2",
+  "rating":"4.85 (18.1K)",
+  "price":"₹399",
+  "time":"60 mins",
+  "desc2":"Pack of 2 Haircut for Men",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1658988755105-b14f5c.png"
+  },
+  {
+  "name":"Kid's Hair Cut",
+  "rating":"4.82 (83.5K)",
+  "price":"₹249",
+  "time":"30 mins",
+  "description":"No more hassle of managing kids outside, safe haircut at home",
+  "desc2":"Expert child stylists for a mess free experience with child friendly equipment",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/home-screen/1616494759820-956198.jpeg",
+  "package":"FOR 3-12 YEARS BOYS"
+  },
+  {
+  "name":"Haircut + Beard Grooming + Massage",
+  "rating":"4.84 (212.9K)",
+  "price":"₹549",
+  "time":"1 hr 5 mins",
+  "description":"Beard Shape & Style",
+  "desc2":"10 min Head Massage",
+  "editpackage":"Edit your package",
+  "desc3":"10 min Head Massage",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Father & Kid's Haircut",
+  "rating":"4.83 (212K)",
+  "price":"₹599",
+  "time":"1 hr 10 mins",
+  "description":"Men's Haircut",
+  "desc2":"Kids Haircut (Boys)",
+  "editpackage":"Edit your package",
+  "desc3":"Kids Haircut (Boys)",
+  "package":"PACKAGE"
+  },
+  {
+  "name":"Hair Color with Product",
+  "rating":"4.82 (1.6K)",
+  "price":"Starts at ₹299",
+  "package":"AMMONIA-FREE PRODUCTS"
+  },
+  {
+  "name":"Hair Color - Application Only",
+  "rating":"4.80 (18.6K)",
+  "price":"₹199",
+  "time":"40 mins",
+  "description":"Even application by professionals especially at the back",
+  "desc2":"Complete coverage of grey hair from tip to root",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1615976235101-6d6cc1.jpeg",
+  "package":"COLOR PROVIDED BY USER"
+  },
+  {
+  "name":"Clean Shave/Moustache Grooming",
+  "rating":"4.83 (56K)",
+  "price":"₹149",
+  "time":"20 mins",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1651660889736-7c3f9d.png"
+  },
+  {
+  "name":"Beard Shaping & Styling",
+  "rating":"4.84 (86.1K)",
+  "price":"₹199",
+  "time":"25 mins",
+  "description":"Get an even beard shape, covering all hard to reach areas",
+  "desc2":"Get customized styles from our professionally trained stylists",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/home-screen/1629192918292-196558.jpeg"
+  },
+  {
+  "name":"Beard Color with Product",
+  "rating":"4.70 (964)",
+  "price":"₹199",
+  "time":"30 mins",
+  "description":"Colour patchy grey beards with a truly natural look",
+  "desc2":"Best quality products to ensure a natural looking color",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/home-screen/1640852398247-6bb793.png"
+  },
+  {
+  "name":"Face & Neck De-Tan",
+  "rating":"4.79 (22.2K)",
+  "price":"Starts at ₹399",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1651666576469-ed8a95.png"
+  },
+  {
+  "name":"Anti-pollution Cleanup",
+  "rating":"4.77 (6K)",
+  "price":"₹699",
+  "time":"35 mins",
+  "description":"Activated charcoal absorb impurities, prevent skin damage",
+  "desc2":"Cleanses excess oil for an energized skin",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/supply/customer-app-supply/1606455920469-0f25d3.jpeg",
+  "package":"FOR DRY SKIN"
+  },
+  {
+  "name":"Oil Reduction Cleanup",
+  "rating":"4.77 (5.1K)",
+  "price":"₹699",
+  "time":"35 mins",
+  "description":"Hydrating antibacterial formula that cleanses dark spots",
+  "desc2":"Powered by antioxidants and vitamins",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/supply/customer-app-supply/1606455911696-01c51e.jpeg"
+  },
+  {
+  "name":"Skin Brightening Facial",
+  "rating":"4.77 (9.3K)",
+  "price":"₹1,399",
+  "time":"1 hr 5 mins",
+  "description":"Brightens skin & evens out skin tone also protects from sun exposure",
+  "desc2":"Includes a deep face stress relief massage",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1651660868642-a7d8db.png"
+  },
+  {
+  "name":"Mulberry & Saffron Facial",
+  "rating":"4.78 (4.2K)",
+  "price":"₹1,399",
+  "time":"1 hr 5 mins",
+  "description":"Transforms and restores dull and dead skin",
+  "desc2":"Natural extracts give a boost of hydration",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1651666605975-8aa0e5.png"
+  },
+  {
+  "name":"Relaxing Head Massage",
+  "rating":"4.83 (95.8K)",
+  "price":"Starts at ₹99",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/luminosity/1651660884996-1c3ba4.png"
+  },
+  {
+  "name":"Neck & Shoulder Massage",
+  "rating":"4.78 (7.5K)",
+  "price":"₹199",
+  "time":"20 mins",
+  "description":"Relaxing Neck & Shoulder Massage",
+  "desc2":"Eases tense and sore muscles",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/supply/customer-app-supply/1609925335989-eccf3c.jpeg",
+  "package":"WFH SPECIAL"
+  },
+  {
+  "name":"Anti Dandruff - Head Massage",
+  "rating":"4.81 (2.5K)",
+  "price":"Starts at ₹149",
+  "description":"Reduces dandruff, itchy and flaky scalp",
+  "desc2":"Onion, curry leaf & neem oil that heal & prevent hair damage",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/home-screen/1631336215990-e722e8.jpeg",
+  "package":"HAIRWASH NOT INCLUDED"
+  },
+  {
+  "name":"Kid's Head Massage",
+  "rating":"4.87 (283)",
+  "price":"Starts at ₹149",
+  "description":"Relaxing, child friendly massage",
+  "desc2":"Helps increase focus & concentration",
+  "image":"https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_96,dpr_1,fl_progressive:steep,q_auto/images/growth/home-screen/1618237381827-c01ef1.jpeg"
+  }
+  ]
+  
+
+
+  // let data=localStorage.setItem('mendata',JSON.stringify(menssalon))
+  // console.log(data)
+  let container=document.getElementById('cont')
+
+   function appenddata() {
+    menssalon.forEach(function(el){
+
+    let div=document.createElement('div')
+
+    let div2=document.createElement('div')
+      
+    let package=document.createElement('h5')
+    package.innerText=el.package
+    if(el.package==undefined){
+      package.innerText=" "
+    }
+    package.style.color='violet'
+
+
+     let name=document.createElement('h3')
+     name.innerText=el.name
+
+
+
+     let rating=document.createElement('span')
+     rating.innerText=el.rating
+
+     
+     let price=document.createElement('p')
+     price.innerText=el.price+" "+'.'+" "+el.time
+     if(el.time==undefined){
+      price.innerText=el.price
+    }
+     let image=document.createElement('img')
+     image.src=el.image
+     if(el.image==undefined){
+      image.src=" "
+    }
+    //  let hr=document.createElement('hr')
+
+
+  div.append(package,name,rating,price)
+  div2.append(image)
+
+  cont.append(div,div2)
+
+    })
+   }
+appenddata()
