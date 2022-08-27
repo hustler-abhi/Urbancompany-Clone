49892665f941b223ee8d7e85a282a5a4d2ef5036
ABHISHEK KUMAR <107480660+hustler-abhi@users.noreply.github.com>
2022-08-27 17:22:02 +0530

    Add files via upload

--- UNIt-3 project/homepage.js
+++ UNIt-3 project/homepage.js
@@ -9,6 +9,48 @@ window.onscroll=function(){
 	}
 
  }
+ let is;
+	let arr = ['salon for men', 'salon for women', 'air purifier','massages for men','hair services for woman','spa for women']
+	function main(func, delay) {
 
+		if (is) {
+			clearTimeout(is)
+		}
+		is = setTimeout(() => {
+			func()
+			 
+		}, delay)
+
+
+	}
+
+	function bounce() {
+
+		let value = document.querySelector('#abhi').value
+		
+		arr.forEach(function (ele,i) {
+			if (ele.includes(value)) {
+				console.log(ele,i)
+				display(ele)
+			}
+		})
+		document.querySelector('#abhi').value=null 
+	}
+
+
+	function display(data){
+		document.querySelector('#display').innerHTML
+		let p=document.createElement('p');
+		p.innerText=data
+		let div=document.createElement('div')
+		div.append(p)
+		document.querySelector('#display').append(div)
+		div.addEventListener('click',()=>{
+			window.location.href='abhi.html'
+		})
+	}
+
+let locaate=localStorage.getItem('location')
+document.querySelector('button').innerText=locaate
 
  
